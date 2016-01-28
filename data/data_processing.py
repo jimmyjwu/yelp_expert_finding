7783bcafe1059c0e44ed44299122288aa41820ee
Jimmy <jimmywu126@gmail.com>
2016-01-27 22:19:05 -0800

    Added script and supporting utilities to combine all user data into one file.

--- data/data_processing.py
+++ data/data_processing.py
@@ -14,6 +14,7 @@ from data_utilities import *
 from data_interface import *
 
 
+# TODO: Refactor the following three functions' write-to-file blocks into one utility function
 def extract_user_average_review_lengths(input_file_name=DEFAULT_RAW_REVIEWS_FILE_NAME, output_file_name=DEFAULT_REVIEW_LENGTHS_FILE_NAME):
 	"""
 	Given a Yelp dataset reviews file (with reviews in JSON format), builds a file:
@@ -33,7 +34,6 @@ def extract_user_average_review_lengths(input_file_name=DEFAULT_RAW_REVIEWS_FILE
 			review = json.loads(review_JSON)
 			total_review_length_and_count_for_user[review['user_id']][0] += len(review['text'].split())
 			total_review_length_and_count_for_user[review['user_id']][1] += 1
-
 
 	# Write each user's average review length to file
 	with open(_processed_data_absolute_path(output_file_name), 'w') as user_review_lengths_file: # Write mode; overwrite old file if it exists
@@ -74,7 +74,6 @@ def extract_user_reading_levels(input_file_name=DEFAULT_RAW_REVIEWS_FILE_NAME, o
 			except UnicodeEncodeError as error:
 				pass
 
-
 	# Write each user's average review reading level to file
 	with open(_processed_data_absolute_path(output_file_name), 'w') as user_reading_levels_file: # Write mode; overwrite old file if it exists
 		
@@ -105,14 +104,65 @@ def combine_all_user_data(
 	input_users_file_name=DEFAULT_RAW_USERS_FILE_NAME,
 	input_review_lengths_file_name=DEFAULT_REVIEW_LENGTHS_FILE_NAME,
 	input_reading_levels_file_name=DEFAULT_READING_LEVELS_FILE_NAME,
+	input_pageranks_file_name=DEFAULT_PAGERANKS_FILE_NAME,
 	output_users_file_name=DEFAULT_COMBINED_USERS_FILE_NAME,
 ):
 	"""
-	Given all data on users (both raw and processed), combines them into a single JSON file of user
-	dictionaries.
+	Given all data on users (both raw and processed), combines them into a single file formatted as follows:
+		ID review_count ... pagerank
+		user_1_ID user_1_review_count ... user_1_pagerank
+			.
+			.
+			.
+		user_N_ID user_N_review_count ... user_N_pagerank
 	"""
-	# TODO: Implement this function
+	# TODO: Check if there are more features provided by the Yelp dataset that are not being added
 
+	user_for_ID = {}
+
+	# Read in basic user attributes
+	with open(_raw_data_absolute_path(input_users_file_name)) as users_file:
+
+		for user_line in users_file:
+			raw_user = json.loads(user_line)
+			user = {
+				'ID': raw_user['user_id'],
+				'review_count': raw_user['review_count'],
+				'average_stars': raw_user['average_stars'],
+				'friend_count': len(raw_user['friends']),
+				'months_member': 12 * (CURRENT_YEAR - int(raw_user['yelping_since'].split('-')[0]) - 1) + (12 - int(raw_user['yelping_since'].split('-')[1])) + 5,
+				'years_elite': len(raw_user['elite']),
+				'fan_count': raw_user['fans'],
+				'funny_vote_count': raw_user['votes']['funny']
+				'useful_vote_count': raw_user['votes']['useful']
+				'cool_vote_count': raw_user['votes']['cool']
+			}
+			user_for_ID[user['ID']] = user
+
+	# Read in average review lengths
+	average_review_length_for_user = read_user_average_review_lengths(input_file_name=input_review_lengths_file_name)
+	for user_ID, average_review_length in average_review_length_for_user.iteritems():
+		user_for_ID[user_ID]['average_review_length'] = average_review_length
+
+	# Read in average reading levels
+	average_reading_level_for_user = read_user_average_reading_levels(input_file_name=input_reading_levels_file_name)
+	for user_ID, average_reading_level in average_reading_level_for_user.iteritems():
+		user_for_ID[user_ID]['average_reading_level'] = average_reading_level
+
+	# Read in PageRanks
+	pagerank_for_user = read_user_pageranks(input_file_name=input_pageranks_file_name)
+	for user_ID, pagerank in pagerank_for_user.iteritems():
+		user_for_ID[user_ID]['pagerank'] = pagerank
+
+	# Write combined user data to file
+	with open(_processed_data_absolute_path(output_users_file_name), 'w') as combined_users_file: # Write mode; overwrite old file if it exists
+
+		# Row 1: attribute names
+		combined_users_file.write( ' '.join(ALL_USER_ATTRIBUTES) + '\n')
+
+		# Rows 2,...,N: users' attribute values
+		for user in user_for_ID.itervalues():
+			combined_users_file.write( ' '.join([str(user[attribute]) for attribute in ALL_USER_ATTRIBUTES]) + '\n')
 
 
 
--- data/data_utilities.py
+++ data/data_utilities.py
@@ -2,8 +2,10 @@
 Utilities specifically for the data processing scripts and data interfaces.
 """
 import os
+from datetime import date
 
 THIS_FILE_PATH = os.path.dirname(__file__)
+CURRENT_YEAR = date.today().year
 
 DEFAULT_RAW_REVIEWS_FILE_NAME = 'yelp_academic_dataset_review.json'
 DEFAULT_RAW_USERS_FILE_NAME = 'yelp_academic_dataset_user.json'
@@ -11,9 +13,41 @@ DEFAULT_RAW_USERS_FILE_NAME = 'yelp_academic_dataset_user.json'
 DEFAULT_REVIEW_LENGTHS_FILE_NAME = 'user_average_review_lengths.txt'
 DEFAULT_READING_LEVELS_FILE_NAME = 'user_reading_levels.txt'
 DEFAULT_PAGERANKS_FILE_NAME = 'user_pageranks.txt'
-DEFAULT_COMBINED_USERS_FILE_NAME = 'combined_users.json'
+DEFAULT_COMBINED_USERS_FILE_NAME = 'combined_users.txt'
 
 DEFAULT_D3_GRAPH_FILE_NAME = 'users_graph.json'
+
+DEFAULT_USER_ATTRIBUTES = [
+	'ID',
+	'review_count',
+	'average_stars',
+	# 'friend_count',
+	'months_member',
+	'years_elite',
+	# 'fan_count',
+	# 'funny_vote_count',
+	# 'useful_vote_count',
+	# 'cool_vote_count',
+	'average_review_length',
+	'average_reading_level',
+	'pagerank',
+]
+
+ALL_USER_ATTRIBUTES = [
+	'ID',
+	'review_count',
+	'average_stars',
+	'friend_count',
+	'months_member',
+	'years_elite',
+	'fan_count',
+	'funny_vote_count',
+	'useful_vote_count',
+	'cool_vote_count',
+	'average_review_length',
+	'average_reading_level',
+	'pagerank',
+]
 
 
 def _raw_data_absolute_path(relative_path):
